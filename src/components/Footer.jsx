8431a89cff54878f10f3a467fc777cf29b80383d
datle-dev <dle.dev@outlook.com>
2024-04-09 11:58:20 -0400

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer>
+      <a target="_blank" href="https://icons8.com/icon/23883/resume">
+        Resume
+      </a>{' '}
+      icon by{' '}
+      <a target="_blank" href="https://icons8.com">
+        Icons8
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
