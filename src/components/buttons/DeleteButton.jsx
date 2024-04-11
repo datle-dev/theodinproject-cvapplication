71a576d0ea010068b1cde806480928835565b716
datle-dev <dle.dev@outlook.com>
2024-04-11 10:14:29 -0400

    Add styling for delete button

--- src/components/buttons/DeleteButton.jsx
+++ src/components/buttons/DeleteButton.jsx
@@ -1,11 +1,15 @@
+import '../../styles/DeleteButton.css';
+import DeleteIcon from '../icons/DeleteIcon';
+
 function DeleteButton({ dataKey, onClick }) {
   return (
     <button
+      className="delete-button"
       type="button"
       onClick={onClick}
       data-key={dataKey}
     >
-      X
+      <DeleteIcon />
     </button>
   );
 }
